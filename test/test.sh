a421340cc79d3fb2d583bba38334fabd13adfdcc
Thomas JOUANNOT <mazerty@gmail.com>
2018-12-12 13:56:47 +0100

    fix nominal test

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@
 # creates tmp directory
 mkdir tmp
 
-# test nominal
+# nominal test: first test fail, then fix, then ok, then second test ok
 ../src/zebr0-icu -c nominal.json -s tmp/nominal_status
 diff tmp/nominal results/nominal
 sed -e '2d' tmp/nominal_status | diff - results/nominal_status
