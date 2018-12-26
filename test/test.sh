daf29f6790ea0fe3c1e98376dc7c9a34e7999cec
Thomas JOUANNOT <mazerty@gmail.com>
2018-12-26 16:39:52 +0100

    better status file comparisons in tests

--- test/test.sh
+++ test/test.sh
@@ -6,20 +6,24 @@
 # creates tmp directory
 mkdir tmp
 
+clean_diff() {
+  sed "s/....-..-..T..:..:..\......./yyyy-mm-ddThh:mm:ss.mmmmmm/g" tmp/$1 | diff - results/$1
+}
+
 # nominal test: first test fail, then fix, then ok, then second test ok
 ../src/zebr0-icu -c nominal.json -s tmp/nominal_status
 diff tmp/nominal results/nominal
-sed -e '2d' tmp/nominal_status | diff - results/nominal_status
+clean_diff nominal_status
 
 # first kind of error: first test fail, then fix, then fail again
 ../src/zebr0-icu -c error1.json -s tmp/error1_status || true
 diff tmp/error1 results/error1
-sed -e '2d' tmp/error1_status | diff - results/error1_status
+clean_diff error1_status
 
 # second kind of error: first test fail, then fix fail
 ../src/zebr0-icu -c error2.json -s tmp/error2_status || true
 diff tmp/error2 results/error2
-sed -e '2d' tmp/error2_status | diff - results/error2_status
+clean_diff error2_status
 
 # cleans tmp directory
 rm -rf tmp
