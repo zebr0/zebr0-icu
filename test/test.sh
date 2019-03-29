895139ab90ac64d74f1209dfb9d890a9160ecc89
Thomas JOUANNOT <mazerty@gmail.com>
2019-03-29 16:14:03 +0100

    now reads the configuration from a configuration directory

--- test/test.sh
+++ test/test.sh
@@ -11,33 +11,34 @@ clean_diff() {
 }
 
 # nominal test: first test fail, then fix, then ok, then second test ok
-../src/heal -c nominal.json -s tmp/nominal_json_status
+../src/heal -d nominal_json -o tmp/nominal_json_status
 diff tmp/nominal_json results/nominal_json
 clean_diff nominal_json_status
 
 # nominal test, with yaml conf
-../src/heal -c nominal.yaml -s tmp/nominal_yaml_status
+../src/heal -d nominal_yaml -o tmp/nominal_yaml_status
 diff tmp/nominal_yaml results/nominal_yaml
 clean_diff nominal_yaml_status
 
 # first kind of error: first test fail, then fix, then fail again
-../src/heal -c error1.json -s tmp/error1_status || true
+../src/heal -d error1_json -o tmp/error1_status || true
 diff tmp/error1 results/error1
 clean_diff error1_status
 
 # second kind of error: first test fail, then fix fail
-../src/heal -c error2.json -s tmp/error2_status || true
+../src/heal -d error2_json -o tmp/error2_status || true
 diff tmp/error2 results/error2
 clean_diff error2_status
 
-echo "../src/heal -c fixing.yaml -s tmp/fixing_status" | at now
+# test of the "fixing" status (requires a certain timing)
+echo "../src/heal -d fixing_yaml -o tmp/fixing_status" | at now
 sleep 1
 clean_diff fixing_status
 sleep 2
 diff tmp/fixing results/fixing
 
 # error when using the default yaml loader
-../src/heal -c error-loader.yaml -s /dev/null
+../src/heal -d error_loader_yaml -o /dev/null
 
 # cleans tmp directory
 rm -rf tmp
