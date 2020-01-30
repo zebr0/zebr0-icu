733d1c716af56e98526f81b7d01d094b8f4a6d9e
Thomas JOUANNOT <mazerty@gmail.com>
2020-01-30 11:17:57 +0100

    we don't pass the args outside of the main program

--- src/heal.py
+++ src/heal.py
@@ -7,8 +7,8 @@ import sys
 import yaml
 
 
-def write_output(status, args):
-    with open(args.output, "w") as output:
+def write_output(status, output):
+    with open(output, "w") as output:
         json.dump({
             "utc": datetime.datetime.utcnow().isoformat(),
             "status": status,
@@ -26,10 +26,10 @@ steps = []
 modes = []
 
 
-def blibli(args):
-    if os.path.isdir(args.directory):
-        for filename in sorted(os.listdir(args.directory)):
-            with open(os.path.join(args.directory, filename)) as file:
+def heal(directory, output):
+    if os.path.isdir(directory):
+        for filename in sorted(os.listdir(directory)):
+            with open(os.path.join(directory, filename)) as file:
                 for step in yaml.load(file, Loader=yaml.BaseLoader):  # uses the yaml baseloader to preserve all strings
                     if not step.get("if") or len(actions.intersection(step.keys())) != 1:
                         print("error: at least one step is missing an 'if' key or hasn't exactly one key amongst " + str(sorted(actions)))
@@ -50,25 +50,25 @@ def blibli(args):
             print("test: " + _if)
             if execute(_if):
                 if _then:
-                    write_output("fixing", args)
+                    write_output("fixing", output)
                     print("test failed! fix: " + _then)
                     execute(_then)
 
                     print("test again: " + _if)
                     if execute(_if):
-                        write_output("ko", args)
+                        write_output("ko", output)
                         print("fix failed!")
                         exit(1)
             else:
                 if _else:
-                    write_output("fixing", args)
+                    write_output("fixing", output)
                     print("test failed! fix: " + _else)
                     execute(_else)
 
                     print("test again: " + _if)
                     if not execute(_if):
-                        write_output("ko", args)
+                        write_output("ko", output)
                         print("fix failed!")
                         exit(1)
 
-            write_output("ok", args)
+            write_output("ok", output)
