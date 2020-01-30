57d60053f28ffc217750ffcdc3402404b0b16f32
Thomas JOUANNOT <mazerty@gmail.com>
2020-01-30 15:39:47 +0100

    extracted the loop reading the file content

--- src/heal.py
+++ src/heal.py
@@ -22,22 +22,26 @@ def execute(command):
     return subprocess.Popen(command, shell=True, stdout=sys.stdout, stderr=sys.stderr).wait() == 0
 
 
+def read(directory):
+    if os.path.isdir(directory):
+        for filename in sorted(os.listdir(directory)):
+            with open(os.path.join(directory, filename)) as file:
+                yield from yaml.load(file, Loader=yaml.BaseLoader)  # uses the yaml baseloader to preserve all strings
+
+
 def heal(directory, output):
     steps = []
     modes = []
 
-    if os.path.isdir(directory):
-        for filename in sorted(os.listdir(directory)):
-            with open(os.path.join(directory, filename)) as file:
-                for step in yaml.load(file, Loader=yaml.BaseLoader):  # uses the yaml baseloader to preserve all strings
-                    if not step.get("if") or len(actions.intersection(step.keys())) != 1:
-                        print("error: at least one step is missing an 'if' key or hasn't exactly one key amongst " + str(sorted(actions)))
-                        exit(1)
-                    if step.get("then-mode"):
-                        if execute(step.get("if")):
-                            modes.append(step.get("then-mode"))
-                    else:
-                        steps.append(step)
+    for step in read(directory):
+        if not step.get("if") or len(actions.intersection(step.keys())) != 1:
+            print("error: at least one step is missing an 'if' key or hasn't exactly one key amongst " + str(sorted(actions)))
+            exit(1)
+        if step.get("then-mode"):
+            if execute(step.get("if")):
+                modes.append(step.get("then-mode"))
+        else:
+            steps.append(step)
 
     for step in steps:
         _if = step.get("if")
