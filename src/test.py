f97e203841311dbee6e5615b0e560910445af4bb
Thomas JOUANNOT <mazerty@gmail.com>
2020-02-27 15:37:42 +0100

    test read

--- src/test.py
+++ src/test.py
@@ -10,5 +10,13 @@ class TestCase(unittest.TestCase):
         self.assertTrue(heal.execute("/bin/true"))
         self.assertFalse(heal.execute("/bin/false"))
 
+    def test_read(self):
+        self.assertListEqual(list(heal.read("../test/modes")),
+                             [{"if": "true", "then-mode": "mode_1"},
+                              {"if": "false", "then-mode": "mode_2"},
+                              {"if-not": "echo 'default' >> tmp/modes", "then": "false"},
+                              {"if-not": "echo 'mode_1' >> tmp/modes", "and-if-mode": "mode_1", "then": "false"},
+                              {"if-not": "echo 'mode_2' >> tmp/modes", "and-if-mode": "mode_2", "then": "false"}])
+
 
 unittest.main(verbosity=2)
