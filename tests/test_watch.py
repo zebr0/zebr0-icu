d566debab625a49dac7bfbf3df745be14a8a2d4f
mazerty <mazerty@gmail.com>
2021-05-23 17:47:11 +0200

    small touches to test_watch

--- tests/test_watch.py
+++ tests/test_watch.py
@@ -2,7 +2,7 @@ import pathlib
 
 import time
 
-import heal
+import heal.watch
 
 READ_CONFIG_FILE_1 = """
 ---
@@ -84,6 +84,14 @@ def test_read_config_ok_empty(tmp_path, capsys):
     assert capsys.readouterr().out == READ_CONFIG_OK_OUTPUT
 
 
+def test_read_config_ok_json(tmp_path, capsys):
+    tmp_path.joinpath("file1.json").write_text('["lorem", "ipsum", "dolor", "sit", "amet"]')
+    tmp_path.joinpath("file2.yml").write_text(READ_CONFIG_FILE_2)
+
+    assert sorted(heal.watch.read_config(tmp_path)) == ["adipiscing", "amet", "consectetur", "dolor", "elit", "ipsum", "lorem", "sit"]
+    assert capsys.readouterr().out == READ_CONFIG_OK_OUTPUT
+
+
 FILTER_MODES_AND_CHECKS_OK_OUTPUT = """
 filtering modes and checks
 done
@@ -148,9 +156,8 @@ def test_filter_current_modes_ok():
     assert heal.watch.filter_current_modes([
         {"mode": "one", "if": "/bin/true"},
         {"mode": "two", "if": "/bin/false"},
-        {"mode": "three", "if": "/bin/false"},
-        {"mode": "four", "if": "/bin/true"}
-    ]) == ["one", "four"]
+        {"mode": "three", "if": "/bin/true"}
+    ]) == ["one", "three"]
 
 
 def test_filter_current_modes_ok_empty():
@@ -188,68 +195,76 @@ def test_filter_current_checks_ok_empty(capsys):
 
 
 def test_directory_has_changed(tmp_path, capsys):
-    watcher = heal.Watcher(tmp_path)
+    watcher = heal.watch.Watcher(tmp_path)
 
+    # upon creation of the watcher, the directory will always show as changed
     assert watcher.directory_has_changed()
     assert watcher.mtime == tmp_path.stat().st_mtime
     assert capsys.readouterr().out == "configuration directory has changed\n"
 
-    time.sleep(0.01)
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
+
+    # if nothing happens to the directory, nothing has changed
     assert not watcher.directory_has_changed()
     assert capsys.readouterr().out == ""
 
-    time.sleep(0.01)
-    tmp_path.joinpath("dummy").write_text("dummy")
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
 
+    # now we write a new file in the directory
+    tmp_path.joinpath("dummy").write_text("dummy")
     assert watcher.directory_has_changed()
     assert capsys.readouterr().out == "configuration directory has changed\n"
 
 
-def test_checks_have_changed(tmp_path, monkeypatch, capsys):
+def test_checks_have_changed(monkeypatch, capsys):
+    # we will ultimately mock filter_modes_and_checks() with different values
+    # so we can mock read_config() and don't need to care about the directory
     monkeypatch.setattr(heal.watch, "read_config", lambda _: None)
+    watcher = heal.watch.Watcher("")
 
-    watcher = heal.Watcher(tmp_path)
-
-    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: ([], []))
+    # first let's test that modes are stored as-is without changing the checks
+    assert watcher.modes == []
+    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: (["mode_1", "mode_2"], []))
     assert not watcher.checks_have_changed()
+    assert watcher.modes == ["mode_1", "mode_2"]
     assert capsys.readouterr().out == ""
 
-    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: (["mode"], []))
-    assert not watcher.checks_have_changed()
-    assert watcher.modes == ["mode"]
-    assert capsys.readouterr().out == ""
-
-    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: (["mode"], ["check1", "check2"]))
+    # then let's add some checks
+    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: ([], ["check_1", "check_2"]))
     assert watcher.checks_have_changed()
-    assert watcher.checks == ["check1", "check2"]
+    assert watcher.checks == ["check_1", "check_2"]
     assert capsys.readouterr().out == "checks have changed\n"
 
-    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: (["mode"], ["check1"]))
+    # then let's remove some checks
+    monkeypatch.setattr(heal.watch, "filter_modes_and_checks", lambda _: ([], ["check_1"]))
     assert watcher.checks_have_changed()
-    assert watcher.checks == ["check1"]
+    assert watcher.checks == ["check_1"]
     assert capsys.readouterr().out == "checks have changed\n"
 
+    # finally, idempotence
     assert not watcher.checks_have_changed()
+    assert watcher.checks == ["check_1"]
     assert capsys.readouterr().out == ""
 
 
-def test_current_modes_have_changed(tmp_path, monkeypatch, capsys):
-    watcher = heal.Watcher(tmp_path)
+def test_current_modes_have_changed(monkeypatch, capsys):
+    watcher = heal.watch.Watcher("")
 
-    monkeypatch.setattr(heal.watch, "filter_current_modes", lambda _: [])
-    assert not watcher.current_modes_have_changed()
-    assert capsys.readouterr().out == ""
-
+    # let's add some modes
     monkeypatch.setattr(heal.watch, "filter_current_modes", lambda _: ["mode1", "mode2"])
     assert watcher.current_modes_have_changed()
     assert watcher.current_modes == ["mode1", "mode2"]
     assert capsys.readouterr().out == "current modes have changed: ['mode1', 'mode2']\n"
 
+    # then let's remove some
     monkeypatch.setattr(heal.watch, "filter_current_modes", lambda _: ["mode1"])
     assert watcher.current_modes_have_changed()
+    assert watcher.current_modes == ["mode1"]
     assert capsys.readouterr().out == "current modes have changed: ['mode1']\n"
 
+    # finally, idempotence
     assert not watcher.current_modes_have_changed()
+    assert watcher.current_modes == ["mode1"]
     assert capsys.readouterr().out == ""
 
 
@@ -340,36 +355,41 @@ done
 def test_refresh_current_checks_if_necessary_ok(tmp_path, capsys):
     config_path = tmp_path.joinpath("config")
     config_path.mkdir()
+    watcher = heal.watch.Watcher(config_path)
 
-    # init
-    watcher = heal.Watcher(config_path)
+    # at first, directory is shown as changed, but nothing else has
     watcher.refresh_current_checks_if_necessary()
     assert watcher.current_checks == []
     assert capsys.readouterr().out == REFRESH_CURRENT_CHECKS_IF_NECESSARY_OUTPUT_1
 
-    # case 1: checks changed, not current modes
-    time.sleep(0.01)
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
+
+    # case 1: checks have changed, current modes haven't
     config_path.joinpath("checks_changed.yaml").write_text(REFRESH_CURRENT_CHECKS_IF_NECESSARY_CHECKS_CHANGED)
     watcher.refresh_current_checks_if_necessary()
     assert watcher.current_checks == [{"check": "just adding a check without mode", "fix": "whatever", "rank": 1}]
     assert capsys.readouterr().out == REFRESH_CURRENT_CHECKS_IF_NECESSARY_OUTPUT_2
 
-    # case 2: checks and current modes changed
-    time.sleep(0.01)
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
+
+    # case 2: both checks and current modes have changed
     config_path.joinpath("checks_and_modes_changed.yaml").write_text(REFRESH_CURRENT_CHECKS_IF_NECESSARY_CHECKS_AND_MODES_CHANGED)
     watcher.refresh_current_checks_if_necessary()
     assert watcher.current_checks == [{"check": "just adding a check without mode", "fix": "whatever", "rank": 1},
                                       {"check": "adding a check with mode", "fix": "whatever", "rank": 2, "when": "basic"}]
     assert capsys.readouterr().out == REFRESH_CURRENT_CHECKS_IF_NECESSARY_OUTPUT_3
 
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
+
     # case 3: new current mode
-    time.sleep(0.01)
     config_path.joinpath("only_modes_changed.yaml").write_text(REFRESH_CURRENT_CHECKS_IF_NECESSARY_ONLY_MODES_CHANGED.format(tmp_path))
     watcher.refresh_current_checks_if_necessary()
     assert watcher.current_checks == [{"check": "just adding a check without mode", "fix": "whatever", "rank": 1},
                                       {"check": "adding a check with mode", "fix": "whatever", "rank": 2, "when": "basic"},
                                       {"check": "adding a check for later", "fix": "whatever", "rank": 10, "when": "special"}]
     assert capsys.readouterr().out == REFRESH_CURRENT_CHECKS_IF_NECESSARY_OUTPUT_4
+
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
 
     # case 4: nothing changed
     watcher.refresh_current_checks_if_necessary()
@@ -378,8 +398,10 @@ def test_refresh_current_checks_if_necessary_ok(tmp_path, capsys):
                                       {"check": "adding a check for later", "fix": "whatever", "rank": 10, "when": "special"}]
     assert capsys.readouterr().out == ""
 
-    # case 5: current modes changed
-    tmp_path.joinpath("flag").touch()
+    time.sleep(0.01)  # let some time pass, to ensure the current mtime has changed
+
+    # case 5: current modes have changed
+    tmp_path.joinpath("flag").touch()  # flag for the "special" mode
     watcher.refresh_current_checks_if_necessary()
     assert watcher.current_checks == [{"check": "just adding a check without mode", "fix": "whatever", "rank": 1},
                                       {"check": "adding a check with mode", "fix": "whatever", "rank": 2, "when": "basic"}]
