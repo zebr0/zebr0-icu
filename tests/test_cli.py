c257e39655223949a4606847ad6503d01e695d30
mazerty <mazerty@gmail.com>
2021-05-23 11:42:35 +0200

    rework test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import signal
 import threading
@@ -6,14 +7,29 @@ import time
 
 import heal
 
+OK_OUTPUT = """
+watching: {0}
+configuration directory has changed
+reading configuration
+done
+filtering modes and checks
+done
+""".lstrip()
 
-def test_ok(tmp_path):
-    bloblo = tmp_path.joinpath("blibli")
-    bloblo.mkdir()
 
-    def blibli():
-        time.sleep(1)
+def test_ok(tmp_path, capsys):
+    configuration_directory = tmp_path.joinpath("conf")
+    configuration_directory.mkdir()
+
+    status_file = tmp_path.joinpath("status.json")
+
+    def delayed_kill():
+        time.sleep(0.5)
         os.kill(os.getpid(), signal.SIGINT)
 
-    threading.Thread(target=blibli).start()
-    heal.main(["-c", str(bloblo), "-s", str(tmp_path.joinpath("bliablia")), "-d", "0.2"])
+    threading.Thread(target=delayed_kill).start()
+
+    heal.main(["-c", str(configuration_directory), "-s", str(status_file), "-d", "0.2"])
+
+    assert capsys.readouterr().out == OK_OUTPUT.format(configuration_directory)
+    assert json.loads(status_file.read_text(encoding="utf-8")).get("status") == "ok"
